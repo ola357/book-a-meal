b4c941667732d183af547be915a76a69dcf21d88
ola357 <alliolaoluwa@gmail.com>
2019-02-18 04:09:28 +0100

    test frame work added

--- server/dist/app.js
+++ server/dist/app.js
@@ -1,16 +1,20 @@
+'use strict';
+
 // const express = require('express');
 // const parties = require('./routes/parties');
 // const offices = require('./routes/offices');
-const express = require('express');
+var express = require('express');
 
-const app = express();
+var app = express();
 
 app.use(express.json());
 // app.use('/api/v1/parties', parties);
 // app.use('/api/v1/offices', offices);
 
 
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 // eslint-disable-next-line no-console
-const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
+var server = app.listen(port, function () {
+  return console.log('Listening on port ' + port + '...');
+});
 module.exports = server;
